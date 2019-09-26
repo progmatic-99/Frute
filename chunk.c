13870f47f299a81032f71fd62fbb790e2ff52a59
progmatic-99 <shubham.upreti@gmail.com>
2019-09-26 23:56:38 +0530

    new branch

---
+++ chunk.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+#include "chunk.h"
+#include "memory.h"
+
+void initChunk(Chunk *chunk)
+{
+	chunk->count = 0;
+	chunk->capacity = 0;
+	chunk->code = NULL;
+}
+
+void freeChunk(Chunk *chunk)
+{
+	FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+	initChunk(chunk);
+}
+
+void writeChunk(Chunk *chunk, uint8_t byte)
+{
+	if(chunk->capacity < chunk->count + 1){
+		int old_capacity = chunk->capacity;
+		chunk->capacity = GROW_CAPACITY(old_capacity);
+		chunk->code = GROW_ARRAY(chunk->code, uint8_t,
+				old_capacity, chunk->capacity);
+	}
+
+	chunk->code[chunk->count] = byte;
+	chunk->count ++;
+}
+
---
+++ chunk.h
@@ -0,0 +1,24 @@
+/* Module to define code representation.
+ * "Chunk" refers to blobs of bytecodes.
+ */
+
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+
+typedef enum{
+	OP_RETURN,
+}Opcode;
+
+typedef struct{
+	int count;
+	int capacity;
+	uint8_t *code;
+}Chunk;
+
+void initChunk(Chunk *chunk);
+void freeChunk(Chunk *chunk);
+void writeChunk(Chunk *chunk, uint8_t byte);
+
+#endif
