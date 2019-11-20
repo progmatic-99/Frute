4a2af92c54306d664845d29d7fcd0083f9f4e217
progmatic-99 <shubham.upreti@gmail.com>
2019-11-20 12:48:35 +0530

    Testing addConstant

--- main.c
+++ main.c
@@ -5,10 +5,13 @@
 int main(int argc, const char * argv[]){
 	Chunk chunk;
 	initChunk(&chunk);
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT);
+    writeChunk(&chunk, constant);
+
 	writeChunk(&chunk, OP_RETURN);
 	freeChunk(&chunk);
 
     disassembleChunk(&chunk, "test chunk");
-
 	return 0;
 }
