41cebfa5235cdb087f4faf940856788598a01c81
progmatic-99 <shubham.upreti@gmail.com>
2019-11-23 11:08:01 +0530

    Added printValue

--- value.h
+++ value.h
@@ -16,6 +16,7 @@ typedef struct {
 void initValueArray(ValueArray *array);
 void writeValueArray(ValueArray *array, Value value);
 void freeValueArray(ValueArray *array);
+void printValue(Value value);
 
 #endif
 
