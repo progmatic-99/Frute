5d0511e65a8869d9e67e09f8a5835850d3a16cc1
Shubham Upreti <shubham.upreti@gmail.com>
2020-03-06 02:47:29 -0500

    Line info

--- src/main.c
+++ src/main.c
@@ -6,10 +6,10 @@ int main(int argc, const char * argv[]){
 	Chunk chunk;
 	initChunk(&chunk);
     int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT);
-    writeChunk(&chunk, constant);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
 
-	writeChunk(&chunk, OP_RETURN);
+	writeChunk(&chunk, OP_RETURN, 123);
 	freeChunk(&chunk);
 
     disassembleChunk(&chunk, "test chunk");
