897e61b06ccbcb97bd377bacdc153a5dc9cd21ab
Shubham Upreti <shubham.upreti@gmail.com>
2020-03-06 02:46:53 -0500

    Foo

--- src/chunk.c
+++ src/chunk.c
@@ -8,26 +8,30 @@ void initChunk(Chunk *chunk)
 	chunk->count = 0;
 	chunk->capacity = 0;
 	chunk->code = NULL;
+    chunk->lines = NULL;
     initValueArray(&chunk->constants);
 }
 
 void freeChunk(Chunk *chunk)
 {
 	FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    FREE_ARRAY(int, chunk->lines, chunk->capacity);
 	initChunk(chunk);
     freeValueArray(&chunk->constants);
 }
 
-void writeChunk(Chunk *chunk, uint8_t byte)
+void writeChunk(Chunk *chunk, uint8_t byte, int line)
 {
 	if(chunk->capacity < chunk->count + 1){
 		int old_capacity = chunk->capacity;
 		chunk->capacity = GROW_CAPACITY(old_capacity);
 		chunk->code = GROW_ARRAY(chunk->code, uint8_t,
 				old_capacity, chunk->capacity);
-	}
+	    chunk->lines = GROW_ARRAY(chunk->lines, int, old_capacity, chunk->capacity);
+    }
 
 	chunk->code[chunk->count] = byte; // Storing the byte
+    chunk->lines[chunk->count] = line; // Storing Line numbers
 	chunk->count ++; // Incrementing counter
 }
 
