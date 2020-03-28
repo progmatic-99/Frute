acbbe3c301f0b6364abf8269ffb0e1a7a26726ed
Shubham Upreti <shubham.upreti@gmail.com>
2020-03-28 09:23:34 +0530

    Decoding

--- vm.c
+++ vm.c
@@ -4,6 +4,7 @@
  * and passing it around.
  * Global variables are bad.
  */
+#include <stdio.h>
 
 #include "common.h"
 #include "vm.h"
@@ -21,18 +22,34 @@ void freeVM()
 static Interpret_Result run()
 {
     #define READ_BYTE() (*vm.ip ++)
+    #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
     for(;;) {
         uint8_t instruction;
         switch(instruction = READ_BYTE())
         {
+            case OP_CONSTANT: {
+                 Value constant = READ_CONSTANT();
+                 printValue(constant);
+                 printf("\n");
+                 break;
+            }
+
+            /*case OP_CONSTANT_LONG: {
+                 Value constant1 = READ_CONSTANT();
+
+                 printValue(constant1);
+                 printf("\n");
+            }*/
+
             case OP_RETURN: {
-                                return INTERPRET_OK;
-                            }
+                 return INTERPRET_OK;
+            }
         }
     }
 
     #undef READ_BYTE
+    #undef READ_CONSTANT
 }
 
 Interpret_Result interpret(Chunk *chunk)
