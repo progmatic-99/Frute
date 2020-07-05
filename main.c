a56ca6710b6364300656f677f05f74d7e0ccbb02
Shubham Upreti <shubham.upreti@gmail.com>
2020-07-05 13:07:08 +0530

    Line number changed

--- main.c
+++ main.c
@@ -7,15 +7,15 @@
 int main(int argc, const char *argv[]) {
     Chunk chunk;
     initChunk(&chunk);
-    int constant = addConstant(&chunk, 83);
+    int constant = addConstant(&chunk, 11);
 
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-    writeChunk(&chunk, OP_RETURN, 123);
-    writeChunk(&chunk, OP_CONSTANT_LONG, 123);
-    writeChunk(&chunk, constant, 123);
-    writeChunk(&chunk, constant, 123);
-    writeChunk(&chunk, OP_RETURN, 123);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+    writeChunk(&chunk, OP_RETURN, 1);
+    writeChunk(&chunk, OP_CONSTANT_LONG, 2);
+    writeChunk(&chunk, constant, 2);
+    writeChunk(&chunk, constant, 2);
+    writeChunk(&chunk, OP_RETURN, 2);
 
     disassembleChunk(&chunk, "Test Chunk");
     freeChunk(&chunk);
